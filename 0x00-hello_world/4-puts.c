9043af08b4825a4cbb02ba1c548145ba246cf2fd
petrosebl <petrostechnologies@gmail.com>
2021-08-26 05:44:18 -0700

    low level programming work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* This program prints Programming is like building a multilingual puzzle */
+/** 
+ *This program prints Programming is like building a multilingual puzzle 
+*/
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");

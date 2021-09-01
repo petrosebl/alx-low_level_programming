a176c038c8d33b316d7e22f758d12e84db04c2dd
petrosebl <petrostechnologies@gmail.com>
2021-09-01 13:43:20 -0700

    alx low level programming nested loops project

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,11 +8,17 @@
  */
 int main(void)
 {
-char word[9] = "Holberton";
-int i;
-
-for (i = 0; i < 9; i++)
-_putchar(word[i]);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+ 
+_putchar('n');
 _putchar('\n');
 
 return (0);
